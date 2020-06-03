b6d519eb0c10e112e1e47cf5d2f5eee0de36fd06
Daniel Halverson <daniel.halverson@hotmail.com>
2020-06-03 11:01:50 -0600

    Add recipe attributes test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -11,5 +11,11 @@ class RecipeTest < Minitest::Test
     assert_instance_of Recipe, recipe1
   end
 
+  def test_it_has_attributes
+    recipe1 = Recipe.new("Mac and Cheese")
+
+    assert_equal "Mac and Cheese". recipe1.name
+  end
+
 
 end
