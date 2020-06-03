a901754232db99983264be2cdb0e6dd7bb7dd5ee
Daniel Halverson <daniel.halverson@hotmail.com>
2020-06-03 11:07:02 -0600

    Add add ingredient test

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -14,8 +14,18 @@ class RecipeTest < Minitest::Test
   def test_it_has_attributes
     recipe1 = Recipe.new("Mac and Cheese")
 
-    assert_equal "Mac and Cheese". recipe1.name
+    assert_equal "Mac and Cheese", recipe1.name
   end
 
+  def test_it_can_add_ingredient
+    recipe1 = Recipe.new("Mac and Cheese")
 
+    assert_equal ({}), recipe1.ingredients_required
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+    expected = {ingredient1 => 6, ingredient2 => 4, ingredient2 => 8}
+
+    assert_equal expected, recipe1.ingredients_required
+  end
 end
