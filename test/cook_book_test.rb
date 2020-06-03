b2144416574dc5f59d3c5410cc18fcad70faa157
Daniel Halverson <daniel.halverson@hotmail.com>
2020-06-03 11:28:51 -0600

    Add cookbook add recipe test

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ingredient'
 require './lib/recipe'
+require './lib/cook_book'
 
 class CookBookTest < Minitest::Test
 
@@ -11,5 +12,16 @@ class CookBookTest < Minitest::Test
     assert_instance_of CookBook, cookbook
   end
 
+  def test_it_can_add_recipe
+    cookbook = CookBook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+
+    assert_equal [], cookbook.recipes
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal [recipe1, recipe2], cookbook.recipes
+  end
 
 end
