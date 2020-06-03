cfca7215859dd2d15838492e74fe703a296d080e
Daniel Halverson <daniel.halverson@hotmail.com>
2020-06-03 11:46:20 -0600

    Add cookbook ingredients test

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -25,4 +25,14 @@ class CookBookTest < Minitest::Test
     assert_equal [recipe1, recipe2], cookbook.recipes
   end
 
+  def test_it_can_show_ingredients
+    cookbook = CookBook.new
+    recipe1 = Recipe.new("Mac and Cheese")
+    recipe2 = Recipe.new("Cheese Burger")
+    cookbook.add_recipe(recipe1)
+    cookbook.add_recipe(recipe2)
+
+    assert_equal ["Cheese", "Macaroni", "Ground Beef", "Bun"], cookbook.ingredients
+  end
+
 end
