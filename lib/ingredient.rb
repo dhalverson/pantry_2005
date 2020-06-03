43524af7cc79e7639ae243112e5dd1f139f1bb6b
Daniel Halverson <daniel.halverson@hotmail.com>
2020-06-03 10:17:18 -0600

    Initial commit

---
+++ lib/ingredient.rb
@@ -0,0 +1,12 @@
+class Ingredient
+
+  attr_reader :name,
+              :unit,
+              :calories
+  def initialize(args)
+    @name = args[:name]
+    @unit = args[:unit]
+    @calories = args[:calories]
+  end
+
+end
---
+++ test/ingredient_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class IngredientTest < Minitest::Test
+
+  def test_it_exists
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+
+    assert_instance_of Ingredient, ingredient1
+  end
+
+  def test_it_has_attributes
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+
+    assert_equal "Cheese", ingredient1.name
+    assert_equal "oz", ingredient1.unit
+    assert_equal 50, ingredient1.calories
+  end
+
+  def test_it_can_stock_check
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    pantry = Pantry.new
+
+
+
+end
