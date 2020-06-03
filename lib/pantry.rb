9b7acf5d05a0c3fb844d3a857021edb0cffdff5f
Daniel Halverson <daniel.halverson@hotmail.com>
2020-06-03 10:29:10 -0600

    Add stock check test and method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,5 +1,12 @@
 class Pantry
 
+  attr_reader :stock
+  def initialize
+    @stock = {}
+  end
 
-
+  def stock_check(ingredient)
+    @stock[ingredient] = {} if @stock[ingredient].nil?
+    @stock[ingredient].values.sum
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -16,6 +16,20 @@ class PantryTest < Minitest::Test
     ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
     pantry = Pantry.new
 
+    assert_equal ({}), pantry.stock
+    assert_equal 0, pantry.stock_check(ingredient1)
+  end
 
+  def test_it_can_restock
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    pantry = Pantry.new
+
+    assert_equal ({}), pantry.stock
+    pantry.restock(ingredient1, 5)
+    pantry.restock(ingredient1, 10)
+
+    assert_equal 15, pantry.stock_check(ingredient1)
+  end
 
 end
