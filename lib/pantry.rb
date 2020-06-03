01f9bc7d4d8cbe610bb88df2e981d05d2f940d0b
Daniel Halverson <daniel.halverson@hotmail.com>
2020-06-03 10:58:07 -0600

    Add restock test and method

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -2,11 +2,15 @@ class Pantry
 
   attr_reader :stock
   def initialize
-    @stock = {}
+    @stock = Hash.new(0)
   end
 
   def stock_check(ingredient)
     @stock[ingredient] = {} if @stock[ingredient].nil?
-    @stock[ingredient].values.sum
+    @stock[ingredient]
+  end
+
+  def restock(ingredient, amount)
+    @stock[ingredient] += amount
   end
 end
